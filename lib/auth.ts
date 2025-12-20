41a2ee12dc97557b4b98524698c13110537fc30d
spartan-readytocode <7007966033abc@gmail.com>
2025-12-20 22:46:11 +0530

    patch : origin config

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,16 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/drizzle/db"
-
+import { db } from "@/drizzle/db";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
     }),
     emailAndPassword: {
-        enabled: true
-    }
+        enabled: true,
+    },
+    trustedOrigins: [
+        "https://stock-data-analyser-chatbot.vercel.app/",
+        "https://www.stock-data-analyser-chatbot.vercel.app/",
+    ],
 });
